c21a09b795c2048ebdadf52629d29fafe673f04d
Adam Ramski <adam.ramski@meelogic.com>
2019-07-05 23:08:47 +0200

    add getClient api

---
+++ src/functions/clients/getClient.js
@@ -0,0 +1,10 @@
+import config from 'config';
+import { wrapper, Storage, verify } from 'utils';
+
+const {
+  sortKeyValues: { CLIENT },
+} = config;
+
+export default wrapper(({ pathParameters: { id } }) =>
+  Storage.get({ pk: id, sk: CLIENT }).then(client => verify.presence(client, 'Client not found')),
+);
