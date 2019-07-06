15984b097323770eb58285193dfdeecfd5734684
Adam Ramski <adam.ramski@meelogic.com>
2019-07-06 15:18:34 +0200

    add syncClient and syncDeparture to createOrder api

--- src/functions/orders/createOrder.js
+++ src/functions/orders/createOrder.js
@@ -1,9 +1,28 @@
 import config from 'config';
+import { isEmpty } from 'lodash';
 import { wrapper, Storage, id, verify } from 'utils';
 
 const {
-  sortKeyValues: { CLIENT, ORDER },
+  sortKeyValues: { CLIENT, DEPARTURE, ORDER },
 } = config;
+
+const syncClient = (clientId, orderId, data) =>
+  Storage.create({ pk: clientId, sk: orderId, ...data });
+
+const syncDeparture = orderDate =>
+  // find departure for given date
+  Storage.query('sk')
+    .eq(DEPARTURE)
+    .where('date')
+    .eq(orderDate)
+    .exec()
+    .then(order =>
+      isEmpty(order)
+        ? // if departure doesn't exist, create one and set orders count to one
+          Storage.create({ pk: id('departure'), sk: DEPARTURE, orders: 1 })
+        : // if departure exists, increment orders count by one
+          Storage.update({ pk: order.pk, sk: DEPARTURE }, { $ADD: { orders: 1 } }),
+    );
 
 export default wrapper(
   ({
@@ -23,25 +42,25 @@ export default wrapper(
   }) =>
     Storage.get({ pk: clientId, sk: CLIENT })
       .then(client => verify.presence(client, 'Client not found'))
-      .then(
-        () =>
-          Storage.create({
-            pk: id('order'),
-            sk: ORDER,
-            date,
-            firstname,
-            lastname,
-            primaryPhonePL,
-            secondaryPhonePL,
-            primaryPhoneNL,
-            secondaryPhoneNL,
-            from,
-            to,
-            clientId,
-          }).then(order =>
-            // add order to client
-            Storage.create({ pk: clientId, sk: order.pk, date, from, to }).then(() => order),
-          ),
-        // todo: after order creation, create departure item in the table and update orders count there
+      .then(() =>
+        Storage.create({
+          pk: id('order'),
+          sk: ORDER,
+          date,
+          firstname,
+          lastname,
+          primaryPhonePL,
+          secondaryPhonePL,
+          primaryPhoneNL,
+          secondaryPhoneNL,
+          from,
+          to,
+          clientId,
+        }).then(order =>
+          Promise.all([
+            syncClient(clientId, order.pk, { date, from, to }),
+            syncDeparture(date),
+          ]).then(() => order),
+        ),
       ),
 );
