a73f251b0e6d390b5ee2522d57158e745f886796
Adam Ramski <adam.ramski@meelogic.com>
2019-07-06 16:22:32 +0200

    add syncDeprature to deleteOrder api

--- src/functions/orders/deleteOrder.js
+++ src/functions/orders/deleteOrder.js
@@ -1,11 +1,33 @@
-import { flatten } from 'lodash';
+import config from 'config';
+import { isEmpty } from 'lodash';
 import { wrapper, Storage } from 'utils';
+
+const {
+  sortKeyValues: { DEPARTURE },
+} = config;
+
+const syncDeparture = orderDate =>
+  // find departure for given date
+  Storage.queryOne('sk')
+    .eq(DEPARTURE)
+    .where('date')
+    .eq(orderDate)
+    .exec()
+    .then(departure => {
+      if (isEmpty(departure)) return Promise.resolve();
+
+      return departure.orders === 1
+        ? Storage.delete({ pk: departure.pk, sk: DEPARTURE })
+        : Storage.update({ pk: departure.pk, sk: DEPARTURE }, { $ADD: { orders: -1 } });
+    });
 
 export default wrapper(({ pathParameters: { id: orderId } }) =>
   Promise.all([
-    Storage.query('pk')
+    Storage.queryOne('pk')
       .eq(orderId)
       .exec(),
     Storage.query('sk').eq(orderId),
   ]),
-).then(results => Storage.batchDelete(flatten(results)));
+).then(([order, clientOrders]) =>
+  Promise.all([Storage.batchDelete([order, ...clientOrders]), syncDeparture(order.date)]),
+);
