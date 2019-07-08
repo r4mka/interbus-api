2ddb8c025d5f7916dab650ac850c9c86405c96a9
Adam Ramski <adam.ramski@meelogic.com>
2019-07-09 00:20:20 +0200

    add getDriverDepartures api

---
+++ src/functions/drivers/getDriverDepartures.js
@@ -0,0 +1,17 @@
+import config from 'config';
+import { wrapper, Storage } from 'utils';
+import { rangeQuery } from 'functions/.common';
+
+const {
+  sortKeyValues: { LIST },
+} = config;
+
+export default wrapper(({ pathParameters: { id: driverId }, queryStringParameters }) =>
+  // todo: add limit controlled by queryStringParam
+  rangeQuery(
+    Storage.query('sk')
+      .using('DateGlobalIndex')
+      .eq(`${LIST}-${driverId}`),
+    queryStringParameters,
+  ).exec(),
+);
