5031a40690501d130e546fcf8ed8b317e232d732
Adam Ramski <adam.ramski@meelogic.com>
2019-07-11 22:26:05 +0200

    add support for YYYY-MM-DD date format to rangeQuery helper

--- src/common/index.js
+++ src/common/index.js
@@ -1,21 +1,21 @@
+import moment from 'moment';
+
 export * from './cars';
 export * from './departures';
 export * from './lists';
 
 export const rangeQuery = (query, { date, from, to } = {}) => {
-  // todo: add params date validation
-
   if (date) {
-    return query.where('date').eq(+date);
+    return query.where('date').eq(moment(+date).valueOf());
   }
   if (from && to) {
-    return query.where('date').between(+from, +to);
+    return query.where('date').between(moment(+from).valueOf(), moment(+to).valueOf());
   }
   if (from) {
-    return query.where('date').ge(+from);
+    return query.where('date').ge(moment(+from).valueOf());
   }
   if (to) {
-    return query.where('date').le(+to);
+    return query.where('date').le(moment(+to).valueOf());
   }
 
   return query;
