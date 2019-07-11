b85adade9b5009fc6fe89a83e798dffabe0af426
Adam Ramski <adam.ramski@meelogic.com>
2019-07-11 22:44:16 +0200

    add option to create order without providing a client

--- src/functions/orders/createOrder.js
+++ src/functions/orders/createOrder.js
@@ -1,5 +1,5 @@
 import config from 'config';
-import { pick } from 'lodash';
+import { has, pick } from 'lodash';
 import { wrapper, Storage, id, verify } from 'utils';
 import { incrementDepartureOrders } from 'common';
 
@@ -7,37 +7,42 @@ const {
   sortKeyValues: { CLIENT, ORDER },
 } = config;
 
-export default wrapper(({ body = {} }) =>
-  Storage.get({ pk: body.clientId, sk: CLIENT })
-    // todo: making orders without providing clientId should be possible as well
-    .then(client => verify.presence(client, 'Client not found'))
-    .then(() =>
-      Storage.create({
-        pk: id(ORDER),
-        sk: ORDER,
-        ...pick(body, [
-          'date',
-          'firstname',
-          'lastname',
-          'primaryPhonePL',
-          'secondaryPhonePL',
-          'primaryPhoneNL',
-          'secondaryPhoneNL',
-          'from',
-          'to',
-          'clientId',
-        ]),
-        // direction, todo: figure out how to compute direction on the fly based on from and to
-      }).then(order =>
-        Promise.all([
-          // add order to client
-          Storage.create({
-            pk: body.clientId,
-            sk: order.pk,
-            ...pick(body, ['date', 'from', 'to']),
-          }),
-          incrementDepartureOrders(body.date),
-        ]).then(() => order),
-      ),
-    ),
-);
+export default wrapper(({ body = {} }) => {
+  const hasClient = has(body, 'clientId');
+  const createOrder = () =>
+    Storage.create({
+      pk: id(ORDER),
+      sk: ORDER,
+      ...pick(body, [
+        'date',
+        'firstname',
+        'lastname',
+        'primaryPhonePL',
+        'secondaryPhonePL',
+        'primaryPhoneNL',
+        'secondaryPhoneNL',
+        'from',
+        'to',
+        'clientId',
+      ]),
+      // direction, todo: figure out how to compute direction on the fly based on from and to
+    }).then(order =>
+      Promise.all([
+        // add order to client
+        hasClient
+          ? Storage.create({
+              pk: body.clientId,
+              sk: order.pk,
+              ...pick(body, ['date', 'from', 'to']),
+            })
+          : {},
+        incrementDepartureOrders(body.date),
+      ]).then(() => order),
+    );
+
+  return hasClient
+    ? Storage.get({ pk: body.clientId, sk: CLIENT })
+        .then(client => verify.presence(client, 'Client not found'))
+        .then(createOrder)
+    : createOrder();
+});
