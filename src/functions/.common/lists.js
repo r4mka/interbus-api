5a8b9bfff2056b634ed96926129045e1bd03d277
Adam Ramski <adam.ramski@meelogic.com>
2019-07-11 19:40:57 +0200

    fix getType helper

--- src/functions/.common/lists.js
+++ src/functions/.common/lists.js
@@ -1,5 +1,5 @@
 import config from 'config';
-import { find } from 'lodash';
+import { includes } from 'lodash';
 import { errors } from 'utils';
 
 const {
@@ -9,7 +9,8 @@ const allowedTypes = [CAR, DRIVER, ORDER];
 
 export const getType = id => {
   const [type] = id.split('-');
-  if (!find(allowedTypes, type)) {
+
+  if (!includes(allowedTypes, type)) {
     throw errors.BadRequest(`${type} type is not supported. Please use one of ${allowedTypes}`);
   }
 
