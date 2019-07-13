8cad380421d3a91679972c22b860ba650d732031
Adam Ramski <adam.ramski@meelogic.com>
2019-07-13 22:45:38 +0200

    add base64Encode, base64Decode and transformResponse helpers to common

--- src/common/index.js
+++ src/common/index.js
@@ -4,6 +4,12 @@ export * from './cars';
 export * from './departures';
 export * from './lists';
 
+export const base64Encode = value => value && Buffer.from(JSON.stringify(value)).toString('base64');
+export const base64Decode = value =>
+  value && JSON.parse(Buffer.from(JSON.stringify(value), 'base64').toString('utf-8'));
+
+export const transformResponse = data => ({ items: data, lastKey: base64Encode(data.lastKey) });
+
 export const rangeQuery = (query, { date, from, to } = {}) => {
   if (date) {
     return query.where('date').eq(moment(+date).valueOf());
